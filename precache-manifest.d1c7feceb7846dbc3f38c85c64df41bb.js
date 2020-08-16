491289bac591cf8ddd7d996804dba210e28af0bf
Shanty97 <shantanu1612@gmail.com>
2020-08-16 13:39:41 +0530

    initial_update

---
+++ precache-manifest.d1c7feceb7846dbc3f38c85c64df41bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3df8d5de03b8895c5f62cb955feaa226",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c21d51a1ebfb9a0a03e6",
+    "url": "/static/css/main.6b655fe2.chunk.css"
+  },
+  {
+    "revision": "f3e3e6796c4016da25af",
+    "url": "/static/js/2.50692a58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.50692a58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c21d51a1ebfb9a0a03e6",
+    "url": "/static/js/main.03166e7e.chunk.js"
+  },
+  {
+    "revision": "df6af45cb4e7a3480417",
+    "url": "/static/js/runtime-main.3bf9fbca.js"
+  }
+]);
